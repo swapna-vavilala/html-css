fa95f56f2459a911aa29528715ad393d1ce54165
Santhosh Vavilala <santhosh.vavilala@hotmail.com>
2021-03-22 00:38:52 +0530

    Add left and right panel to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LeftPanelComponent } from './components/left-panel/left-panel.component';
+import { RightPanelComponent } from './components/right-panel/right-panel.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftPanelComponent,
+    RightPanelComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
